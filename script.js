b87514b7d8f7ceb56aa56b22148cebfc9ae6bb00
MarioVilche <marioevilche@gmail.com>
2025-08-20 19:53:37 -0300

    optimisación

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 let darkMode = confirm("Quiere activar el modo oscuro?")
 
 let tipoDeFondo = document.querySelector(".light")
-let header = document.querySelector(".header")
-let item = document.querySelectorAll(".item")
 let botonCambioDeModo = document.querySelector("#modo")
 
 if (darkMode) {
+
     tipoDeFondo.classList.add("dark")
-    header.classList.add("header-dark")
-    item.forEach(item => {
-        item.classList.add("item-dark")
-    });
+
     botonCambioDeModo.innerHTML = `Modo claro <i class="fa-solid fa-sun"></i>`
+
 }
 
 botonCambioDeModo.addEventListener("click", () => {
+
     tipoDeFondo.classList.toggle("dark")
-    header.classList.toggle("header-dark")
-    item.forEach(item => {
-        item.classList.toggle("item-dark")
-    })
 
     setTimeout(() => {
         if (tipoDeFondo.classList.contains("dark")) {
